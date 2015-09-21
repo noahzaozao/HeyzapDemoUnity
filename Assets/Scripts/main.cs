da22ef1703449431a2fee58cb766abd559c78f19
Noah <234082230@qq.com>
2015-09-21 14:04:49 +0800

    init

---
+++ Assets/Scripts/main.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Your Publisher ID is: 78a1f195d49dd1fb055962b88a3c912d
+		HeyzapAds.start("78a1f195d49dd1fb055962b88a3c912d", HeyzapAds.FLAG_NO_OPTIONS);
+		HeyzapAds.showMediationTestSuite ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
